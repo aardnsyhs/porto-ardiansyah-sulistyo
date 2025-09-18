edd118477bb870de30e741f5d46afa12940221c0
aardnsyhs <ardiansyahdian755@gmail.com>
2025-09-18 15:06:59 +0700

    refactor: change photo profile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import profilePhoto from "@/assets/profile-photo.jpg";
+import profilePhoto from "@/assets/profile-photo.jpeg";
 import TypingEffect from "./TypingEffect";
 import ParticleBackground from "./ParticleBackground";
 
