609a7eb7b3462c604ff89ed0a6999188e267c228
aardnsyhs <ardiansyahdian755@gmail.com>
2025-09-18 11:02:11 +0700

    refactor: delete custom cursor

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,19 @@
-import Navigation from '@/components/Navigation';
-import HeroSection from '@/components/HeroSection';
-import AboutSection from '@/components/AboutSection';
-import SkillsSection from '@/components/SkillsSection';
-import StatsSection from '@/components/StatsSection';
-import ExperienceSection from '@/components/ExperienceSection';
-import PortfolioSection from '@/components/PortfolioSection';
-import ContactSection from '@/components/ContactSection';
-import Footer from '@/components/Footer';
-import CommandPalette from '@/components/CommandPalette';
-import CustomCursor from '@/components/CustomCursor';
-import { ThemeProvider } from '@/contexts/ThemeContext';
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+import StatsSection from "@/components/StatsSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+import CommandPalette from "@/components/CommandPalette";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const Index = () => {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-background">
-        <CustomCursor />
         <Navigation />
         <main>
           <HeroSection />
