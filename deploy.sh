1b3d5bbeb2fa22f80cd94031492ced305d06f3ef
Ardiansyah Sulistyo <iyan@srv1164110.hstgr.cloud>
2025-11-30 13:33:10 +0000

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -e
 APP_DIR="/var/www/porto/porto-ardiansyah-sulistyo"
 cd "$APP_DIR"
 git pull origin main
-/home/iyan/.nvm/versions/node/v24.11.1/bin/npm install
-/home/iyan/.nvm/versions/node/v24.11.1/bin/npm run build
+/usr/bin/npm install
+/usr/bin/npm run build
 echo "Deploy done at $(date)"
